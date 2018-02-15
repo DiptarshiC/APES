917eb131c0474e1a89a8b3d99b93519cf958ce09
Diptarshi Chakraborty <31807400+DiptarshiC@users.noreply.github.com>
2018-02-15 14:15:00 -0700

    Different tasks can now write to same file

--- HW3/pthread_cpu.c
+++ HW3/pthread_cpu.c
@@ -52,19 +52,33 @@
 struct thread_info{
 
   int job_code;
-  FILE *fptr;
+  char filename[20];
 
   };
 
-/*An array of strucutres is being created.
-This array shall be passed to the individual threads*/
-struct thread_info myinfo[NUM_CHILD];
+/*An array of structures is being created.
+This array shall be passed to the individual threads
+Here the File *ptr will be common for both the threads
+The task no. for 1 will be 1 and for the other will be 2
+*/
+struct thread_info myinfoarray[NUM_CHILD];
+
+
+/*This is a globally defined mutex
+that allows us to lock resources
+I have named it so because it is
+use to lock a file
+*/
+pthread_mutex_t mutexfile;
+
+
 
 void periodic_task(int signum)
 {
 
-
-	printf("I am printing stats \n");
+	FILE *ptr2=fopen(myinfoarray[1].filename,"a");
+	printf(".This child thread 2 running.I am printing stats \n");
+	fprintf(ptr2,".This child thread 2 running.I am printing stats \n");
 
 		FILE *fptr;
 		char c;
@@ -86,12 +100,18 @@ void periodic_task(int signum)
 		{
 
 			printf("%c",c);
+			fprintf(ptr2,"%c",c);
 			c=fgetc(fptr);
 		}
 
+		printf("\n\n");
+		fprintf(ptr2,"\n\n");
+
 		fclose(fptr);
+		fclose(ptr2);
 
 }
+
 /**
 *@Function pointer name:child_play
 *
@@ -110,22 +130,61 @@ void periodic_task(int signum)
 
 void * child_play(void * parm)
 {
-  	int *jobname;
-  	jobname=(int*) parm;
-	printf("I am child task (%d) \n",*(jobname));
-	printf("My thread id is (%ld)\n",pthread_self());
 
 
- 	if(*(jobname)==1)
+	struct thread_info *MY_INFO;
+
+	MY_INFO=(struct thread_info*) parm;
+
+
+ 	if(MY_INFO->job_code==1)
     	{
+	//	 pthread_mutex_lock(&mutexfile);
+
+		FILE *ptr=fopen(MY_INFO->filename,"a");
+
 		//printf("This is child task 1\n");
+		//fprintf(ptr,"My job is to search text in a file\n");
+		printf("I am child thread (%d) \n",MY_INFO->job_code);
+		fprintf(ptr,"I am child thread (%d) \n",MY_INFO->job_code);
+       		printf("My thread id is (%ld)\n",pthread_self());
+		fprintf(ptr,"My thread id is (%ld)\n",pthread_self());
 		printf("My job is to search text in a file\n");
-    	}
-  	else if(*(jobname)==2)
-    	{
-		//printf("This is child task 2\n");
-		printf("My job is to print CPU stats\n");
+		fprintf(ptr,"My job is to search text in a file\n");
 
+		printf("\n");
+		printf("\n");
+		fprintf(ptr,"\n");
+		fprintf(ptr,"\n");
+		fclose(ptr);
+
+	//	pthread_mutex_unlock(&mutexfile);
+
+
+    	}
+  	if(MY_INFO->job_code==2)
+    	{
+
+
+
+
+		FILE *ptr1=fopen(MY_INFO->filename,"a");
+
+
+		printf("I am child thread (%d) \n",MY_INFO->job_code);
+		fprintf(ptr1,"I am child thread (%d) \n",MY_INFO->job_code);
+	        printf("My thread id is (%ld)\n",pthread_self());
+		fprintf(ptr1,"My thread id is (%ld)\n",pthread_self());
+
+		printf("My job is to print CPU stats\n");
+		fprintf(ptr1,"My job is to print CPU stats\n");
+
+
+		printf("\n");
+		fprintf(ptr1,"\n");
+		printf("\n");
+		fprintf(ptr1,"\n");
+		fclose(ptr1);
 		struct sigaction sa;
 		struct itimerval timer;
 
@@ -148,23 +207,72 @@ void * child_play(void * parm)
 
 
 		/*Starts a virtual timer.It counts down whenever this process is executing*/
-
 		setitimer(ITIMER_VIRTUAL,&timer,NULL);
 
 		/*Do busy work*/
 		while(1);
+	//	fclose(ptr1);
+	//	pthread_mutex_unlock(&mutexfile);
+
+
 
    	}
-
-
+	pthread_exit(NULL);
 }
+
+
+/**
+*@Function name:main
+*
+*@parameters:void
+*
+*@return:none
+*
+*@Description:This is the main function. The main
+*thread here is the parent thread that creates the
+*two children threads.It also takes the file name
+*to write to from the user.
+*
+*/
+
+
+
 
 void main()
 {
 
 
-	printf("I am a parent thread\n");
-	printf("My thread id is (%ld)\n",pthread_self());
+
+	printf("Please enter the name of the file you would like to create\n");
+	scanf("%s",myinfoarray[0].filename);
+
+	for(int i=0;i<20;i++)
+	{
+
+		myinfoarray[1].filename[i]=myinfoarray[0].filename[i];
+
+	}
+
+	printf("The filename that you entered is\n");
+
+	for(int i=0;i<20;i++)
+	{
+
+		printf("%c",myinfoarray[1].filename[i]);
+	}
+
+	printf("\n\n");
+
+	myinfoarray[0].job_code=1;
+	myinfoarray[1].job_code=2;
+
+//	pthread_mutex_lock(&mutexfile);
+
+
+
+//	fclose(ptr);
+//	pthread_mutex_unlock(&mutexfile);
+
 
 	timer_t timerid;
 	struct sigevent sev;
@@ -179,12 +287,31 @@ void main()
 	int jobcode1=1;//Depending on the job codes the child thread does its job.This is the job code for child thread1
 	int jobcode2=2;//Depending onn the job code the child thread does its job.This is the job code for child thread2
 
-	printf("This is parent task \n");
-        printf("My job is to create two child tasks\n");
 
+	FILE *ptr=fopen(myinfoarray[0].filename,"a");
+
+	printf("This is parent thread \n");
+	fprintf(ptr,"This is parent thread \n");
+	printf("My thread id is (%ld)\n",pthread_self());
+	fprintf(ptr,"My thread id is (%ld)\n",pthread_self());
+
+        printf("My job is to create two child threads\n");
+	printf("\n");
+	printf("\n");
+
+	fprintf(ptr,"My job is to create two child threads\n");
+	fprintf(ptr,"\n");
+        fprintf(ptr,"\n");
+	fclose(ptr);
+
+
+	pthread_mutex_init(&mutexfile,NULL);
+	pthread_attr_t attr;
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr,PTHREAD_CREATE_JOINABLE);
 
 	/*The line below creates the first thread*/
-	thread_status=pthread_create(&thread1,NULL,child_play,(void*) &jobcode1);
+	thread_status=pthread_create(&thread1,&attr,child_play,(void*) &myinfoarray[0]);
 
 	if(thread_status)
   	{
@@ -195,7 +322,7 @@ void main()
 
 
 	/*The line below creates the second thread*/
-	thread_status=pthread_create(&thread2,NULL,child_play,(void*) &jobcode2);
+	thread_status=pthread_create(&thread2,&attr,child_play,(void*) &myinfoarray[1]);
 
 	if(thread_status)
   	{
@@ -203,7 +330,7 @@ void main()
 		exit(EXIT_FAILURE);
   	}
 
-
+	pthread_attr_destroy(&attr);
 	/* The code below ensures that the main thread or
 	the father thread is waiting till the child threads,
 	both thread1 and thread2 get completed*/
@@ -227,9 +354,9 @@ void main()
 
 
 
-	/* The code below ensures that thread1 and
-	thread2 are killed by SIGUSR1,
-	*/
+/*	 The code below ensures that thread1 and
+	thread2 are killed by SIGUSR1,*/
+
 
 	thread_status=pthread_kill(thread1,SIGUSR1);
 
