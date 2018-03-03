93d8e2c6233f42ab3e526045d18db71e37342551
root <root@beaglebone.localdomain>
2018-03-03 22:33:36 +0000

    Added two tasks running concurrently

--- HW4/Kthread.c
+++ HW4/Kthread.c
@@ -25,14 +25,16 @@
 #include <linux/kthread.h>
 
 static struct task_struct *thread_st;
+static struct task_struct *thread_st1;
+
 // Function executed by kernel thread
 static int thread_fn(void *unused)
 {
-    while (1)
-    {
+    
+    
         printk(KERN_INFO "This is baby thread\n");
         
-    }
+    
     printk(KERN_INFO "Thread Stopping\n");
     do_exit(0);
     return 0;
@@ -40,24 +42,39 @@ static int thread_fn(void *unused)
 // Module Initialization
 static int __init init_thread(void)
 {
-    printk(KERN_INFO "Creating Thread\n");
+    printk(KERN_INFO "Creating Thread1\n");
     //Create the kernel thread with name 'mythread'
-    thread_st = kthread_create(thread_fn, NULL, "mythread");
+    thread_st = kthread_run(thread_fn, NULL, "mythread");
     if (thread_st)
 	{
         printk("Thread1 Created successfully\n");
 	}
-    else{
+    else
+	{
         printk(KERN_INFO "Thread creation failed\n");
-	}	
+	}
+
+	 printk(KERN_INFO "Creating Thread2\n");
+    //Create the kernel thread2 with name 'mythread'
+    thread_st1 = kthread_run(thread_fn, NULL, "mythread2");
+    if (thread_st1)
+        {
+        printk("Thread2 Created successfully\n");
+        }
+    else
+	{
+        printk(KERN_INFO "Thread creation failed\n");
+        }
+
+	
     return 0;
 }
 // Module Exit
 static void __exit cleanup_thread(void)
 {
-    printk("Cleaning Up\n");
+    printk(KERN_INFO "Cleaning Up\n");
 }
 
 module_init(init_thread);
 module_exit(cleanup_thread);
-//exit
+
