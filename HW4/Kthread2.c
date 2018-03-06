07d68a86b25e4d22f2cc2e4dfe33200ce57e0d07
root <root@beaglebone.localdomain>
2018-03-06 02:44:16 +0000

    Final requirements done

--- HW4/Kthread2.c
+++ HW4/Kthread2.c
@@ -1,32 +1,36 @@
-
 /** 
 *@file: Kthread.c 
 * 
-*@Description: use the kthread API to create 
+*@Description: Use the kthread API to create 
 *a kernel module with a second thread that allows the two to communicate 
 *via queues (kfifo).
 *The first thread should send information to the 
-*second thread on a timed interval through the fifo. The second thread 
-*should take data from the kfifo and print it to the kernel logger.The 
-*info you should pass should relate to the currently scheduled processes 
-*in the rbtree. What was the ID and vruntime of the previous, current, 
-*and next PID 
+*second thread on a timed interval through the kfifo.The second thread 
+*takes data from the kfifo and prints it to the kernel logger every
+* 100 ms.The info is the PID of the current, next and previous process.  
 * 
 *@Author : Diptarshi Chakraborty 
 * 
-*@Date : February 3rd 2018 * */
+*@Date : February 3rd 2018 
+*/
 
-//program begins here
-#include <linux/module.h>
-#include <linux/kernel.h>
-#include <linux/delay.h>
+#include <linux/interrupt.h>
+#include <linux/err.h>
+#include <linux/irq.h>
+#include <linux/clk.h>
+#include <linux/list.h>
 #include <linux/kthread.h>
+#include <linux/sched.h>
+#include <linux/rtmutex.h>
+#include <linux/hrtimer.h>
+#include <linux/delay.h>
+#include <linux/module.h>
 #include <linux/kfifo.h>
 #include <linux/mutex.h>
 #include <linux/proc_fs.h>
 #include <linux/timer.h>
 #include <linux/sched.h>
-
+#include <linux/kernel.h>
 
 
 struct task_struct *task;
@@ -34,40 +38,28 @@ struct task_struct *task1;
 struct task_struct *previous_task;
 struct task_struct *next_task;
 
-
-/*Create a timer of type list_timer*/
-static struct timer_list my_timer;
-
 /* fifo size in elements (ints)The fifo 
 	stores 3 elements of size integer */
-#define FIFO_SIZE	32
+#define FIFO_SIZE	128
 
 
-/* lock for procfs read access */
-static DEFINE_MUTEX(read_lock);
 
-/* lock for procfs write access */
-static DEFINE_MUTEX(write_lock);
 
 /*This function declares a statically allocated FIFO*/
 static DECLARE_KFIFO(test, unsigned char,FIFO_SIZE);
 static DECLARE_KFIFO(test1,unsigned int,FIFO_SIZE);
-static DECLARE_KFIFO(test2,unsigned long long ,FIFO_SIZE);
-
-
-
-
-
+static DECLARE_KFIFO(test2,unsigned long long int,FIFO_SIZE);
 static int kern_logger(void *unused);
 static int writer(void *unused);
 
-
+static struct task_struct *thread_st;
+static struct task_struct *thread_st1;
 
 MODULE_LICENSE("Dual BSD/GPL");
 MODULE_AUTHOR("DIPTARSHI CHAKRABORTY");
 
-static struct task_struct *thread_st;
-static struct task_struct *thread_st1;
+ 
+static struct task_struct *thread_10ms;
 
 /**
 *@function:writer
@@ -83,68 +75,90 @@ static struct task_struct *thread_st1;
 *
 *@return:(int)
 */
-
-static int writer(void *unused)
+ 
+static int writer(void *arg) 
 {
-    task=current;
-    unsigned char	buf[6];
+unsigned char	buf[6];
     unsigned char	i, j;
     unsigned int	ret;
     unsigned char 	p;	
     unsigned char       g[10];
     unsigned char       g1;
-    u64			vc;
-    u64			vp;
-    u64			vn;
-
+    unsigned long long int vc;
+    unsigned long long int vp;
+    unsigned long long int vn;
 	 INIT_KFIFO(test);
-	 INIT_KFIFO(test1);    	
-    	 printk(KERN_INFO "\n\n");
-	
-  
- previous_task= list_entry(task->tasks.prev, struct task_struct, tasks);
-  
- next_task=list_entry(task->tasks.next, struct task_struct, tasks);
+	 INIT_KFIFO(test1);
+	 INIT_KFIFO(test2);    	
 
-/* vc=task->se.vruntime;
- vp=previous_task->se.vruntime;   
- vn=next_task->se.vruntime;*/  
+
+
+ktime_t timeout = ktime_get();
+while(!kthread_should_stop()) 
+{
+
+task = current;
+previous_task= list_entry(task->tasks.prev, struct task_struct, tasks);
+  
+next_task=list_entry(task->tasks.next, struct task_struct, tasks);
+
  
 printk(KERN_INFO "This is writer thread \n");
 printk(KERN_INFO "The name of the current task is %s\n",(task->comm));
 printk(KERN_INFO "The PID of the current task is %d\n",(task->pid));
+printk(KERN_INFO "The vruntime of the current task is %llu\n",(task->se.vruntime));
+
 
 
 printk(KERN_INFO "The name of the previous task is %s\n",(previous_task->comm));
 printk(KERN_INFO "The PID of the previous task is %d\n",(previous_task->pid));
+printk(KERN_INFO "The vruntime of the previous task is %llu\n",(previous_task->se.vruntime));
 
 
 printk(KERN_INFO "The name of the next task is %s\n",(next_task->comm));
 printk(KERN_INFO "The PID of the next task is %d\n",(next_task->pid));
+printk(KERN_INFO "The vruntime of the next task is %llu\n",(next_task->se.vruntime));
 
 
 
 
-    printk(KERN_INFO "Now let us put data into the fifo. \n");
+
+printk(KERN_INFO "Now let us put data into the fifo\n");
+
+    
     p=kfifo_put(&test, 'd');
     p=kfifo_put(&test, 'i');
     p=kfifo_put(&test, 'p');
    	
-    p=kfifo_put(&test1, current->pid);
+    p=kfifo_put(&test1, task->pid);
     p=kfifo_put(&test1, previous_task->pid);
     p=kfifo_put(&test1, next_task->pid);
    
-    p=kfifo_put(&test2, current->se.vruntime);
+    p=kfifo_put(&test2, task->se.vruntime);
     p=kfifo_put(&test2, previous_task->se.vruntime);
     p=kfifo_put(&test2, next_task->se.vruntime);
 
 	
+    
+
+	//Create the kernel thread2 with name 'mythread'
+	thread_st1 = kthread_run(kern_logger, NULL, "Kern_logger");
+	if (thread_st1)
+        {
+        printk("Kern logger Created successfully\n");
+        }
+	else
+	{
+        printk(KERN_INFO "Kern logger thread creation failed\n");
+        }
     printk(KERN_INFO "Writer thread stopping\n");
     printk(KERN_INFO "\n\n");
-    do_exit(0);
-    return 0;
+    timeout = ktime_add_us(timeout, 10000);
+    __set_current_state(TASK_UNINTERRUPTIBLE);
+    schedule_hrtimeout_range(&timeout, 100, HRTIMER_MODE_ABS);
+    }
+	return 0;	
 }
-
 
 /**
 *@function:kern_logger
@@ -162,7 +176,7 @@ static int kern_logger(void *unused)
 {
 	unsigned char g[10];
 	unsigned int g1[10];
-	unsigned long long g2[10];
+	unsigned long long int g2[10];
 	char p;
 	task=current;
 	printk(KERN_INFO "\n\n");
@@ -175,11 +189,11 @@ static int kern_logger(void *unused)
 	printk(KERN_INFO "The PID of the task is %d\n",(task->pid));
 
 	p=kfifo_get(&test, g);
-        printk(KERN_INFO "buf : %c\n",g[0]);
+       
         p=kfifo_get(&test, g);
-        printk(KERN_INFO "buf : %c\n",g[0]);
+       
         p=kfifo_get(&test, g);
-        printk(KERN_INFO "buf : %c\n",g[0]);
+
 
 p=kfifo_get(&test1,g1);
 printk(KERN_INFO "The pid of the current task from buffer : %d\n",g1[0]);
@@ -190,11 +204,11 @@ printk(KERN_INFO "The pid of the next task from buffer : %d\n",g1[0]);
 
 
 p=kfifo_get(&test2,g2);
-printk(KERN_INFO "The vruntime of the current task from buffer : %llu\n",g2[0]);
+printk(KERN_INFO "The vruntime of the current task from buffer:%llu\n",g2[0]);
 p=kfifo_get(&test2,g2);
-printk(KERN_INFO "The vruntime of the previous task from buffer : %llu\n",g2[0]);
+printk(KERN_INFO "The vruntime of the previous task from buffer:%llu\n",g2[0]);
 p=kfifo_get(&test2,g2);
-printk(KERN_INFO "The vruntime of the next task from buffer : %llu\n",g2[0]);
+printk(KERN_INFO "The vruntime of the next task from buffer: %llu\n",g2[0]);
 
 
 
@@ -207,7 +221,7 @@ printk(KERN_INFO "The vruntime of the next task from buffer : %llu\n",g2[0]);
 }
 
 /**
-*@function:init_thread
+*@function: bus_timer_interrupt_init
 *
 *@description:This function creates the two threads
 *       kern_logger and writer.
@@ -218,37 +232,21 @@ printk(KERN_INFO "The vruntime of the next task from buffer : %llu\n",g2[0]);
 *@return:(int)
 */
 
-static int __init init_thread(void)
+
+ 
+int __init  bus_timer_interrupt_init(void) 
 {
-	printk(KERN_INFO "Creating Writer thread\n");
-	//Create the kernel thread with name 'mythread'
-	thread_st = kthread_run(writer, NULL, "Writer");
-	if (thread_st)
-	{
-        printk("Writer thread Created successfully\n");
-	}
-    	else
-	{
-        printk(KERN_INFO "Writer thread creation failed\n");
-	}
-
-	printk(KERN_INFO "Creating Kern Logger thread\n");
-	//Create the kernel thread2 with name 'mythread'
-	thread_st1 = kthread_run(kern_logger, NULL, "Kern_logger");
-	if (thread_st1)
-        {
-        printk("Kern logger Created successfully\n");
-        }
-	else
-	{
-        printk(KERN_INFO "Kern logger thread creation failed\n");
-        }
-
+	struct sched_param param = { .sched_priority = MAX_RT_PRIO - 1 };
 	
-    return 0;
+	thread_10ms = kthread_create(writer, NULL, "writer");
+	if (IS_ERR(thread_10ms)) {
+	printk(KERN_ERR "RT Failed to create RT thread\n");
+	return -ESRCH;
+	}
+	sched_setscheduler(thread_10ms, SCHED_FIFO, &param);
+	wake_up_process(thread_10ms);
+	return 0;
 }
-
-
 /**
 *@function:cleanup_thread
 *
@@ -261,14 +259,15 @@ static int __init init_thread(void)
 *@return:void
 */
 
-static void __exit cleanup_thread(void)
-{
-	int ret;
+
  
-	ret = del_timer( &my_timer );
-    	printk(KERN_INFO "Cleaning Up\n");
+void __exit  cleanup_thread(void) 
+{
+	kthread_stop(thread_10ms);
+	printk(KERN_INFO "RT Thread removed.\n");
 }
 
-module_init(init_thread);
+   
+module_init(bus_timer_interrupt_init);
 module_exit(cleanup_thread);
 
