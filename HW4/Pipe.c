90fb4aaa3be0774029c99993d20ccaca653c0e07
root <root@beaglebone.localdomain>
2018-03-07 00:09:13 +0000

    Adding Pipe source file

--- HW4/Pipe.c
+++ HW4/Pipe.c
@@ -1,226 +1,131 @@
+/** 
+*@file: Pipe.c 
+* 
+*@Description:Use the pipe IPC to create 
+* a pipe to communicate between a parent
+* and a child  
+* 
+*@Author : Diptarshi Chakraborty 
+* 
+*@Date : February 3rd 2018 
+*/
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 #include <sys/wait.h>
+#include <sys/types.h>
 
-/* max  receiving buffer size; Note: no check or
-			 enforcement is made on this value*/
-#define BUF_SIZE 256
-
-
-struct mystruct
-
+//Structure of the data which is communicated between the parent and the child using pipes.
+typedef struct payload
 {
+  char string[30];
+  bool led_state;
+} payload_t;
 
-	char mesg[40];
-	int USR_LED;
-};
+void errExit(char *);
 
-#define	MSG_LEN	64
-
-/**
-*@function:main
-*
-*@description:The main function creates two 
-*		child processes
-*
-*@input_param:None
-*
-*@return_type:void
-*
-*/
-
-void main()
+int main(void)
 {
+  pid_t Child_Pid = 0;
+  payload_t data;
 
-    int pfd1[2];
-    int pfd2[2];
+     /*File descriptor 1*/	
+  int parent_to_child[2];     
+   /*file descriptor 2*/
+  int child_to_parent[2];     
 
-    ssize_t numRead = -1;
-    /* Note: working under the assumption that the messages
-       are of equal length*/
-    const char* messageOne = "Hello from child ONE.\n";
-    const char* messageTwo = "Hello from child TWO.\n";
 
-    const unsigned int commLen = strlen(messageOne) + 1;
 
-    char buf[BUF_SIZE];
+  if(pipe(parent_to_child) == -1)
+    errExit("pipe parent_to_child");
 
-    if (pipe(pfd1) == -1)
-    {
-        printf("Error opening pipe 1!\n");
-        exit(1);
-    }
+  if(pipe(child_to_parent) == -1)
+    errExit("pipe child_to_parent");
 
-    if (pipe(pfd2) == -1)
-    {
-        printf("Error opening pipe 2!\n");
-        exit(1);
-    }
+  printf("Parent process is forking child process.\n");
 
-    printf("Piped opened with success. Forking ...\n");
+  switch (Child_Pid = fork())
+  {
+     case -1: 
+     errExit("fork");
+     break;
 
-    // child 1
-    switch (fork())
-    {
-        case -1:
-            printf("Error forking child 1!\n");
-            exit(1);
+     case 0: 
+     printf(" Child process has  Forked.\n");
+     if(close(child_to_parent[0]) == -1)      
+        errExit("close child_to_parent read");
 
-        case 0:
-            printf("\nChild 1 executing...\n");
-            /* close reading end of first pipe */
-            if (close(pfd1[0]) == -1)
-            {
-                printf("Error closing reading end of pipe 1.\n");
-                _exit(1);
-            }
-            /* close writing end of second pipe */
-            if (close(pfd2[1]) == -1)
-            {
-                printf("Error closing writing end of pipe 2.\n");
-                _exit(1);
-            }
+     if(close(parent_to_child[1]) == -1)     
+        errExit("close parent_to_child write");
+     printf(" Handled pipe descriptors.\n");
 
-            /* write to pipe 1 */
-            if (write(pfd1[1], messageOne, commLen) != commLen)
-            {
-                printf("Error writing to pipe 1.\n");
-                _exit(1);
-            }
+     if(read(parent_to_child[0], &data, sizeof(payload_t)) == -1)
+        errExit("read parent_to_child");
 
-            if (close(pfd1[1]) == -1)
-            {
-                printf("Error closing writing end of pipe 1.\n");
-                _exit(1);
-            }
+      printf(" the child has received string: \"%s\". Received LED State: %s.\n", data.string, data.led_state ? "true" : "false");
 
-            /* reding from pipe 2 */
-            numRead = read(pfd2[0], buf, commLen);
-            if (numRead == -1)
-            {
-                printf("Error reading from pipe 2.\n");
-                _exit(1);
-            }
+      if(close(parent_to_child[0]) == -1)    
+        errExit("close parent_to_child read");
+      printf(" read end of parent_to_child closed.\n");
 
-            if (close(pfd2[0]) == -1)
-            {
-                printf("Error closing reding end of pipe 2.\n");
-                _exit(1);
-            }
+      strcat(data.string, " World");
+      data.led_state = !data.led_state;
 
-            printf("Message received child ONE: %s", buf);
-            printf("Exiting child 1...\n");
-            _exit(0);
+      printf("This is child here.Piping modified string: \"%s\". Modified Received LED State: %s.\n", data.string, data.led_state ? "true" : "false");
+      if(write(child_to_parent[1], &data, sizeof(payload_t)) == -1)
+        errExit("write child_to_parent");
 
-        default:
-            break;
-    }
+      if(close(child_to_parent[1]) == -1)        //close child_to_parent write
+        errExit("close child_to_parent write");
+      printf(" Piping successful. Closed write end of child_to_parent.\n");
 
-    // child 2
-    switch (fork())
-    {
-        case -1:
-            printf("Error forking child 2!\n");
-            exit(1);
-        case 0:
-            printf("\nChild 2 executing...\n");
-            /* close reading end of second pipe */
-            if (close(pfd2[0]) == -1)
-            {
-                printf("Error closing reading end of pipe 2.\n");
-                _exit(1);
-            }
-            /* close writing end of first pipe */
-            if (close(pfd1[1]) == -1)
-            {
-                printf("Error closing writing end of pipe 1.\n");
-                _exit(1);
-            }
+      break;
 
-            /* read from the first pipe */
-            if (read(pfd1[0], buf, commLen) == -1)
-            {
-                printf("Error reading from pipe 1.\n");
-                _exit(EXIT_FAILURE);
-            }
+    default: 
+      if(close(child_to_parent[1]) == -1)  /*close child_to_parent write*/
+        errExit("close child_to_parent write");
 
-            if (close(pfd1[0]) == -1)
-            {
-                printf("Error closing reading end of pipe 1.\n");
-                _exit(EXIT_FAILURE);
-            }
+      if(close(parent_to_child[0]) == -1)  /*close parent_to_child read*/
+        errExit("close parent_to_child read");
+      printf(" Handled pipe descriptors.\n");
 
-            /* write to the second pipe */
-            if (write(pfd2[1], messageTwo, commLen) != commLen)
-            {
-                printf("Error writing to the pipe.");
-                _exit(EXIT_FAILURE);
-            }
+      strcpy(data.string, "Hello");
+      data.led_state = false;
 
-            if (close(pfd2[1]) == -1)
-            {
-                printf("Error closing writing end of pipe 2.");
-                _exit(EXIT_FAILURE);
-            }
+      printf(" Piping string: \"%s\". LED State: %s.\n", data.string, data.led_state ? "true" : "false");
 
-            printf("Message received child TWO: %s", buf);
-            printf("Exiting child 2...\n");
-            _exit(EXIT_SUCCESS);
+      if(write(parent_to_child[1], &data, sizeof(payload_t)) == -1)
+        errExit("write parent_to_child");
 
-        default:
-            break;
-    }
+      if(close(parent_to_child[1]) == -1)       
+        errExit("close parent_to_child write");
+      printf(" Piping successful. Closed write end of parent_to_child.\n");
 
-    printf("Parent closing pipes.\n");
+      if(read(child_to_parent[0], &data, sizeof(payload_t)) == -1)
+        errExit("read child_to_parent");
 
-    if (close(pfd1[0]) == -1)
-    {
-        printf("Error closing reading end of the pipe.\n");
-        exit(EXIT_FAILURE);
-    }
+      printf(" Received string: \"%s\". Received LED State: %s.\n", data.string, data.led_state ? "true" : "false");
 
-    if (close(pfd2[1]) == -1)
-    {
-        printf("Error closing writing end of the pipe.\n");
-        exit(EXIT_FAILURE);
-    }
+      if(close(child_to_parent[0]) == -1)        
+        errExit("close child_to_parent read");
+      printf(" Piping successful. Closed read end of child_to_parent.\n");
 
-    if (close(pfd2[0]) == -1)
-    {
-        printf("Error closing reading end of the pipe.\n");
-        exit(EXIT_FAILURE);
-    }
+      break;
+  }
 
-    if (close(pfd1[1]) == -1)
-    {
-        printf("Error closing writing end of the pipe.\n");
-        exit(EXIT_FAILURE);
-    }
+  exit(EXIT_SUCCESS);
 
-    printf("Parent waiting for children completion...\n");
-    if (wait(NULL) == -1)
-    {
-        printf("Error waiting.\n");
-        exit(EXIT_FAILURE);
-    }
+}
 
-    if (wait(NULL) == -1)
-    {
-        printf("Error waiting.\n");
-        exit(EXIT_FAILURE);
-    }
 
-    printf("Parent finishing.\n");
-    exit(EXIT_SUCCESS);
-
-	
-
-	
+void errExit(char *strError)
+{
+  perror(strError);
+  exit(EXIT_FAILURE);
 }	
-
-	
 	
 
 
