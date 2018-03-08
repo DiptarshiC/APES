0fe4206c4c769262d8c29477bf4fbc7dcc68850f
root <root@beaglebone.localdomain>
2018-03-08 06:40:07 +0000

    Header file commit

--- Project1/includes/i2c.h
+++ Project1/includes/i2c.h
@@ -1,11 +1,36 @@
 /**
 *@file: i2c.h
 *
-*@description: 
-*
+*@description: This header file provides an abstraction
+*	 	for reading and writing values in the i2c
+*	 	registers.
 *
 *@Date:2nd March 2017 
 *
 *@Author:Diptarshi Chakraborty and Connor Shapiro
 */
+
+#ifndef __I2C_H__
+#define __I2C_H__
+
+
+
+
+
+
+
+
+
+#endif /*__I2C_H__ */
+
+
+
+
+
+
+
+
+
+
+
 
