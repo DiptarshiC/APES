bcec4fdd34d88cc79cd70c5074eff56289c6ef10
root <root@beaglebone.localdomain>
2018-03-09 01:05:23 +0000

    i2h commit

--- Project1/includes/i2c.h
+++ Project1/includes/i2c.h
@@ -13,7 +13,41 @@
 #ifndef __I2C_H__
 #define __I2C_H__
 
+#include <stdint.h>
 
+
+/**
+* @function i2c_read
+*
+* @brief writes into an i2c register
+*
+* Given an address and a pointer to that address,
+*       it writes to the address
+*
+* @param uint8_t address : address of the register to read from
+*              
+*
+* @return uint8_t *
+*/
+uint8_t * i2c_read(uint8_t address);
+
+
+/**
+* @function i2c_write
+*
+* @brief writes into an i2c register
+*
+* Given an address and a pointer to that address,
+* 	it writes to the address
+*
+* @param __user data Pointer to a data item fro
+*		user space
+* @param value value to write to the location
+*
+* @return void.
+*/
+
+void i2c_write(uint8_t address, uint8_t *data);
 
 
 
