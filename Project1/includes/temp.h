5aa8036cb47034c6e5d18ab00d720efebcabd045
root <root@beaglebone.localdomain>
2018-03-10 01:34:16 +0000

    Temp task headers

--- Project1/includes/temp.h
+++ Project1/includes/temp.h
@@ -36,12 +36,12 @@ typedef enum
 
 
 /**
-* @function i2c_read
+* @function write_ptr_reg
 *
-* @brief reads from an i2c register
+* @brief writes to temp sensor ptr reg
 *
-* Given an address it will read from the 
-*       address
+*  
+*      
 *
 * @param  uint8_t data:data to write to the pointer
 *                 register
@@ -50,6 +50,135 @@ typedef enum
 */
 
 temperature_e_t write_ptr_reg (uint8_t data);
+
+/**
+* @function write_conf_reg
+*
+* @brief writes into the config reg
+*
+* 
+*       
+*
+* @param  uint8_t data:data to write to the config
+*                 register
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t write_conf_reg (uint16_t data);
+
+
+/**
+* @function write_tlow_reg
+*
+* @brief writes into the tlow reg
+*
+* @param  uint8_t data:data to write to the tlow
+*                 register
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t write_tlow_reg (uint16_t data);
+
+
+/**
+* @function write_thi_reg
+*
+* @brief writes into the thi reg
+*
+*
+* @param  uint8_t data:data to write to the thi
+*                 register
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t write_thi_reg (uint16_t data);
+
+/**
+* @function read_temperature_reg
+*
+* @brief reads from the temp reg
+*
+*
+* @param  uint8_t *data:ptr to store data
+*                 
+* @return temperature_e_t
+*/
+
+temperature_e_t read_temperature_reg (uint16_t *data);
+
+
+/**
+* @function read_config_reg
+*
+* @brief reads from the config reg
+*
+*
+* @param  uint8_t data:pointer to store data
+*                 
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t read_config_reg (uint16_t *data);
+
+
+/**
+* @function tlow_config_reg
+*
+* @brief reads from the tlow reg
+*
+*
+* @param  uint8_t data:pointer to store data
+*
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t read_tlow_reg (uint16_t *data);
+
+/**
+* @function read_thi_reg
+*
+* @brief reads from the thi reg
+*
+*
+* @param  uint8_t data:pointer to store data
+*
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t read_thi_reg (uint16_t *data);
+
+/**
+* @function read_thi_reg
+*
+* @brief reads from the thi reg
+*
+* @param  uint8_t data:pointer to store data
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t read_thi_reg (uint16_t *data);
+
+/**
+* @function timeout
+*
+* @brief reads from Regularly gathers new
+*	 temperature data after a set
+*	 period of time without new
+*	data.
+*
+* @param  void
+*
+* @return temperature_e_t
+*/
+
+temperature_e_t timeout (void);
 
 
 #endif /*__TEMP_H__ */
