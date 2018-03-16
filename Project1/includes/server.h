3cc2bf14a0952cadfc845537ca58fd18be099d52
root <root@beaglebone.localdomain>
2018-03-16 02:33:00 +0000

    Started with server.c

---
+++ Project1/includes/server.h
@@ -0,0 +1,46 @@
+/**
+*@file: server.h
+*
+*@description: This header file provides an abstraction
+*	 	for reading and writing values in the i2c
+*	 	temp sensor.
+*
+*@Date:2nd March 2017 
+*
+*@Author:Diptarshi Chakraborty and Connor Shapiro
+*/
+
+
+
+#ifndef __SERVER_H__
+#define __SERVER_H__
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif /*__SERVER_H__ */
+
+
+
+
+
+
+
+
+
+
