4b30da2417fd1a31ed35fe5324e3be1f5550d345
Diptarshi Chakraborty <dich4615@colorado.edu>
2018-04-22 04:27:55 -0600

    Initialized the UART Peripheral

--- Project2/src/comms.c
+++ Project2/src/comms.c
@@ -5,6 +5,12 @@
  *
  *  Author: Diptarshi Chakraborty and Connor Shapiro
  *
+ *  For UART we are using
+ *
+ *  PC7         U5Tx
+ *
+ *  PC6         U5Rx
+ *
  *
  */
 
@@ -30,6 +36,45 @@ vCommunicationsTask(void *pvParameters)
 
 void initialize_UART()
 {
+    uint32_t ui32SysClock = SysCtlClockFreqSet((SYSCTL_XTAL_25MHZ |
+                                                  SYSCTL_OSC_MAIN |
+                                                  SYSCTL_USE_OSC), 12000000);
+
+        /*
+         Enable the peripherals used by this example.
+         The UART itself needs to be enabled, as well as the GPIO port
+         containing the pins that will be used.
+        */
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_UART5);
+        SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOC);
+
+        /*
+         Configure the GPIO pin muxing for the UART function.
+         This is only necessary if your part supports GPIO pin function muxing.
+         Study the data sheet to see which functions are allocated per pin.
+         TODO: change this to select the port/pin you are using
+        */
+
+          GPIOPinConfigure(GPIO_PC7_U5TX);
+          GPIOPinConfigure(GPIO_PC6_U5RX);
+
+        /*
+          Since GPIO A0 and A1 are used for the UART function, they must be
+          configured for use as a peripheral function (instead of GPIO).
+          TODO: change this to match the port/pin you are using
+        */
+
+          GPIOPinTypeUART(GPIO_PORTC_BASE, GPIO_PIN_6 | GPIO_PIN_7);
+
+        /*
+          Configure the UART for 115,200, 8-N-1 operation.
+          This function uses SysCtlClockGet() or ui32SysClock to get the system clock
+          frequency.  This could be also be a variable or hard coded value
+          instead of a function call.
+        */
+
+          UARTConfigSetExpClk(UART0_BASE, ui32SysClock, 115200,(UART_CONFIG_WLEN_8 | UART_CONFIG_STOP_ONE | UART_CONFIG_PAR_NONE));
+
 
 }
 
